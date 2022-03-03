d9c5a6961b5d64bec4f61a69186530e1919ee94a
techlife112 <techlife1999@gmail.com>
2022-03-03 18:19:06 +0100

    sd

---
+++ js/script.js
@@ -0,0 +1,53 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var countDownDate = new Date("April 5, 2022 15:37:25 UTC").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  if (days <10) {
+    days = "0" +days;
+  }
+  if (hours <10) {
+    hours = "0" +hours;
+  }
+  if (minutes <10) {
+    minutes = "0" +minutes;
+  }
+  if (seconds <10) {
+    seconds = "0" +seconds;
+  }
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("daysd").innerHTML = "d";
+  document.getElementById("hoursh").innerHTML = "h";
+  document.getElementById("minutesm").innerHTML = "m";
+  document.getElementById("secondss").innerHTML = "s";
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
